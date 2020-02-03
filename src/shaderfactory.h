3314c93a02ab7f78081c503f2cb141a6d8dc8b7c
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2020-02-03 10:46:56 +0100

    Basics

---
+++ src/shaderfactory.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "glslang/Public/ShaderLang.h"
+
+namespace megamol {
+namespace shaderfactory {
+
+class shaderfactory {
+public:
+    shaderfactory(shaderfactory const&) = delete;
+    shaderfactory& operator=(shaderfactory const&) = delete;
+    shaderfactory(shaderfactory&&) = delete;
+    shaderfactory& operator=(shaderfactory&&) = delete;
+
+    // https://codereview.stackexchange.com/questions/173929/modern-c-singleton-template
+    static auto& instance() {
+        static shaderfactory factory;
+        return factory;
+    }
+
+private:
+    shaderfactory() { glslang::InitializeProcess(); }
+
+    ~shaderfactory() { glslang::FinalizeProcess(); }
+};
+
+} // end namespace shaderfactory
+} // end namespace megamol
