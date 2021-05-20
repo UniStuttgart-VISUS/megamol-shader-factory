0e054bd553dce2fdf544026ce60b54fc41ec4640
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-05-20 11:23:42 +0200

    remove resources from options

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -13,6 +13,125 @@
 #include <tuple>
 
 /*
+ * default resource limits for glslang compiler
+ * Adapted from: https://github.com/google/shaderc/blob/main/libshaderc_util/src/resources.cc
+ * License: Apache 2.0
+ */
+const TBuiltInResource default_resource_limits = {
+    /*.maxLights = */ 8,        // From OpenGL 3.0 table 6.46.
+    /*.maxClipPlanes = */ 6,    // From OpenGL 3.0 table 6.46.
+    /*.maxTextureUnits = */ 2,  // From OpenGL 3.0 table 6.50.
+    /*.maxTextureCoords = */ 8, // From OpenGL 3.0 table 6.50.
+    /*.maxVertexAttribs = */ 16,
+    /*.maxVertexUniformComponents = */ 1024,
+    /*.maxVaryingFloats = */ 60, // From OpenGLES 3.1 table 6.44.
+    /*.maxVertexTextureImageUnits = */ 16,
+    /*.maxCombinedTextureImageUnits = */ 80,
+    /*.maxTextureImageUnits = */ 16,
+    /*.maxFragmentUniformComponents = */ 1024,
+
+    /*.maxDrawBuffers = */ 8,
+
+    /*.maxVertexUniformVectors = */ 256,
+    /*.maxVaryingVectors = */ 15,
+    /*.maxFragmentUniformVectors = */ 256,
+    /*.maxVertexOutputVectors = */ 16,  // maxVertexOutputComponents / 4
+    /*.maxFragmentInputVectors = */ 32, // maxFragmentInputComponents / 4
+    /*.minProgramTexelOffset = */ -8,
+    /*.maxProgramTexelOffset = */ 7,
+    /*.maxClipDistances = */ 8,
+    /*.maxComputeWorkGroupCountX = */ 65535,
+    /*.maxComputeWorkGroupCountY = */ 65535,
+    /*.maxComputeWorkGroupCountZ = */ 65535,
+    /*.maxComputeWorkGroupSizeX = */ 1024,
+    /*.maxComputeWorkGroupSizeY = */ 1024,
+    /*.maxComputeWorkGroupSizeZ = */ 64,
+    /*.maxComputeUniformComponents = */ 1024,
+    /*.maxComputeTextureImageUnits = */ 16,
+    /*.maxComputeImageUniforms = */ 8,
+    /*.maxComputeAtomicCounters = */ 8,
+    /*.maxComputeAtomicCounterBuffers = */ 8,
+    /*.maxVaryingComponents = */ 60,
+    /*.maxVertexOutputComponents = */ 64,
+    /*.maxGeometryInputComponents = */ 64,
+    /*.maxGeometryOutputComponents = */ 128,
+    /*.maxFragmentInputComponents = */ 128,
+    /*.maxImageUnits = */ 8,
+    /*.maxCombinedImageUnitsAndFragmentOutputs = */ 8, // ?
+    /*.maxCombinedShaderOutputResources = */ 8,
+    /*.maxImageSamples = */ 0,
+    /*.maxVertexImageUniforms = */ 0,
+    /*.maxTessControlImageUniforms = */ 0,
+    /*.maxTessEvaluationImageUniforms = */ 0,
+    /*.maxGeometryImageUniforms = */ 0,
+    /*.maxFragmentImageUniforms = */ 8,
+    /*.maxCombinedImageUniforms = */ 8,
+    /*.maxGeometryTextureImageUnits = */ 16,
+    /*.maxGeometryOutputVertices = */ 256,
+    /*.maxGeometryTotalOutputComponents = */ 1024,
+    /*.maxGeometryUniformComponents = */ 1024,
+    /*.maxGeometryVaryingComponents = */ 60, // Does not seem to be defined
+                                             // anywhere, set equal to
+                                             // maxVaryingComponents.
+    /*.maxTessControlInputComponents = */ 128,
+    /*.maxTessControlOutputComponents = */ 128,
+    /*.maxTessControlTextureImageUnits = */ 16,
+    /*.maxTessControlUniformComponents = */ 1024,
+    /*.maxTessControlTotalOutputComponents = */ 4096,
+    /*.maxTessEvaluationInputComponents = */ 128,
+    /*.maxTessEvaluationOutputComponents = */ 128,
+    /*.maxTessEvaluationTextureImageUnits = */ 16,
+    /*.maxTessEvaluationUniformComponents = */ 1024,
+    /*.maxTessPatchComponents = */ 120,
+    /*.maxPatchVertices = */ 32,
+    /*.maxTessGenLevel = */ 64,
+    /*.maxViewports = */ 16,
+    /*.maxVertexAtomicCounters = */ 0,
+    /*.maxTessControlAtomicCounters = */ 0,
+    /*.maxTessEvaluationAtomicCounters = */ 0,
+    /*.maxGeometryAtomicCounters = */ 0,
+    /*.maxFragmentAtomicCounters = */ 8,
+    /*.maxCombinedAtomicCounters = */ 8,
+    /*.maxAtomicCounterBindings = */ 1,
+    /*.maxVertexAtomicCounterBuffers = */ 0,
+
+    /*.maxTessControlAtomicCounterBuffers = */ 0,
+    /*.maxTessEvaluationAtomicCounterBuffers = */ 0,
+    /*.maxGeometryAtomicCounterBuffers = */ 0,
+
+    /*.maxFragmentAtomicCounterBuffers = */ 1,
+    /*.maxCombinedAtomicCounterBuffers = */ 1,
+    /*.maxAtomicCounterBufferSize = */ 32,
+    /*.maxTransformFeedbackBuffers = */ 4,
+    /*.maxTransformFeedbackInterleavedComponents = */ 64,
+    /*.maxCullDistances = */ 8,
+    /*.maxCombinedClipAndCullDistances = */ 8,
+    /*.maxSamples = */ 4,
+    /* .maxMeshOutputVerticesNV = */ 256,
+    /* .maxMeshOutputPrimitivesNV = */ 512,
+    /* .maxMeshWorkGroupSizeX_NV = */ 32,
+    /* .maxMeshWorkGroupSizeY_NV = */ 1,
+    /* .maxMeshWorkGroupSizeZ_NV = */ 1,
+    /* .maxTaskWorkGroupSizeX_NV = */ 32,
+    /* .maxTaskWorkGroupSizeY_NV = */ 1,
+    /* .maxTaskWorkGroupSizeZ_NV = */ 1,
+    /* .maxMeshViewCountNV = */ 4,
+    /* .maxDualSourceDrawBuffersEXT = */ 1,
+
+    /*.limits = */
+    {
+        /*.nonInductiveForLoops = */ 1,
+        /*.whileLoops = */ 1,
+        /*.doWhileLoops = */ 1,
+        /*.generalUniformIndexing = */ 1,
+        /*.generalAttributeMatrixVectorIndexing = */ 1,
+        /*.generalVaryingIndexing = */ 1,
+        /*.generalSamplerIndexing = */ 1,
+        /*.generalVariableIndexing = */ 1,
+        /*.generalConstantMatrixVectorIndexing = */ 1,
+    }};
+
+/*
  * Adapted from : https://github.com/google/shaderc/blob/main/libshaderc_util/src/compiler.cc
  * License : Apache 2.0
  */
@@ -93,7 +212,7 @@ std::string megamol::shaderfactory::compiler::preprocess(
     auto inc = options.get_includer();
     std::string output;
     auto const success =
-        shader.preprocess(options.get_resource_limits(), version, profile, true, false, EShMsgDefault, &output, inc);
+        shader.preprocess(&default_resource_limits, version, profile, true, false, EShMsgDefault, &output, inc);
 
     if (!success) {
         throw std::runtime_error(std::string("Error preprocessing shader:\n") + shader.getInfoLog());
