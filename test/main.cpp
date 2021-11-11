efd0d4bc1e19818b04cb8966f537290f53092bc9
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-11-11 13:47:01 +0100

    run test without opengl

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
-#define MSF_OPENGL_INCLUDE_GLAD
 #include "msf/ShaderFactory.h"
-#include "msf/ShaderFactoryOptionsOpenGL.h"
+#include "msf/ShaderFactoryOptions.h"
 
 int main() {
-    msf::ShaderFactoryOptionsOpenGL options({"../test"}, "__AMD__");
-    // options.addDefinition("__AMD__");
+    msf::ShaderFactoryOptions options({"../test"});
+    options.addDefinition("__AMD__");
 
     msf::ShaderFactory factory;
     auto content = factory.preprocess("../test/test.vert.glsl", options);
